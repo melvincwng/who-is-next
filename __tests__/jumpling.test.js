166b7cacd3ddf446dbf998e32b49c8998fa5cd81
melvincwng <melvincwng@users.noreply.github.com>
2021-03-03 17:58:24 +0800

    Implement Mongoose Testing

--- __tests__/jumpling.test.js
+++ __tests__/jumpling.test.js
@@ -2,6 +2,7 @@ const app = require("../src/app");
 const request = require("supertest");
 const dbHandlers = require("../test/dbHandler")
 const Jumpling = require("../src/models/jumpling.model");
+const { response } = require("../src/app");
 
 describe("jumplings", () => {
 
@@ -33,64 +34,106 @@ describe("jumplings", () => {
   });
 
   describe("GET /jumplings/presenter", () => {
-    it("should return a random jumpling", async () => {});
+    it("should return a randomly chosen jumpling from the DB", async () => {
+        const response = await request(app).get("/jumplings/presenter").expect(200);
+        expect(Object.keys(response.body).length).toEqual(3)
+    });
   });
 
   describe("GET /jumplings/:name", () => {
-    it("should retrieve jumpling with requested name", async () => {});
+    it("should retrieve jumpling with requested name", async () => {
+      const requestedJumpling = { name: "Person 1" };
+      const response = await request(app).get("/jumplings/Person 1").expect(200);
+      expect(response.body).toMatchObject(requestedJumpling)
+    });
   });
 
   describe("POST /jumplings", () => {
-    it("should create new jumpling if fields are valid", async () => {});
+    it("should create new jumpling & save it in DB & return it if fields are valid", async () => {
+        const newJumpling = { name: "Testing123" };
+        const response = await request(app).post("/jumplings").send(newJumpling).expect(201);
+        expect(response.status).toEqual(201);
+        expect(response.body).toMatchObject(newJumpling)
+    });
 
-    it("should throw error if name is empty", async () => {});
+    it("should throw error if name is empty", async () => {
+        const newJumpling = { name: "" };
+        const response = await request(app).post("/jumplings").send(newJumpling).expect(500);
+        expect(response.status).toBe(500);
+    });
 
-    it("should throw error if name is too short", async () => {});
+    it("should throw error if name is too short", async () => {
+        const newJumpling = { name: "a" };
+        const response = await request(app).post("/jumplings").send(newJumpling).expect(500);
+        expect(response.status).toBe(500);
+    });
   });
 
   describe("PUT /jumplings/:id", () => {
-    it("should modify specified jumpling if fields are valid", async () => {});
+    it("should modify specified jumpling if fields are valid and return it", async () => {
+      const jumpling = await Jumpling.findOne({ name: "Person 1" });
+      const newJumpling = { name: "Testing123" };
 
-    it("should throw error if name is empty", async () => {});
+      const response = await request(app)
+      .put(`/jumplings/${jumpling._id}`)
+      .send(newJumpling) 
+      .expect(200)
 
-    it("should throw error if request body is not json", async () => {});
+      expect(response.status).toBe(200);
+      expect(response.body.name).toBe('Testing123')
+      expect(response.body).toMatchObject(newJumpling)
+
+    });
+
+    it("should throw error if name is empty", async () => {
+      const jumpling = await Jumpling.findOne({ name: "Person 1" });
+      const newJumpling = { name: "" };
+
+      const response = await request(app)
+      .put(`/jumplings/${jumpling._id}`)
+      .send(newJumpling) 
+      .expect(500)
+
+      expect(response.status).toBe(500);
+
+    });
+
+    it("should throw error if request body is not json", async () => {
+      const jumpling = await Jumpling.findOne({ name: "Person 1" });
+      const newJumpling = "";
+
+      const response = await request(app)
+      .put(`/jumplings/${jumpling._id}`)
+      .send(newJumpling) 
+      .expect(400)
+
+      expect(response.status).toBe(400);
+    });
   });
 
   describe("DELETE /jumplings/:id", () => {
-    it("should delete jumpling if jumpling exists", async () => {});
+    it("should delete jumpling if it exists and then return it", async () => {
+          const jumpling = await Jumpling.findOne({ name: "Person 1" });
+          const expectedDeletedJumpling = { name: "Person 1" };
 
-    it("should throw error if jumpling does not exist", async () => {});
+          const response = await request(app)
+          .delete(`/jumplings/${jumpling._id}`)
+          .expect(200)
+
+          expect(response.status).toBe(200)
+          expect(response.body).toMatchObject(expectedDeletedJumpling)
+    });
+
+    it("should throw error if jumpling does not exist", async () => {
+        const nonExistingJumplingID = "603f3axxxxxxxxx";
+        const nonExistingJumpling = { name: "Person 3 NA" };
+        const jumpling = await Jumpling.findOne(nonExistingJumpling); //would not be able to find the jumpling => hence the jumpling variable will be a falsy value
+        if (!jumpling) {
+          const response = await request(app).delete(`/jumplings/${nonExistingJumplingID}`).expect(500)
+          expect(response.status).toBe(500);
+        }
+
+    });
   });
 });
 
-/*
-it("POST /jumplings should return the person object you posted", async () => {
-    const newJumpling = { "name": "Jeff"};
-    const expectedResponse = { "id": 1, "name": "Jeff" };
-    const response = await request(app).post("/jumplings").send(newJumpling).expect(201);
-    expect(response.body).toMatchObject(expectedResponse)
-});
-
-it("GET /jumplings/:name should return the person object with the correct name", async () => {
-    const expectedResponse = { "id": 1, "name": "Jeff" };
-    const response = await request(app).get("/jumplings/Jeff").expect(200);
-    expect(response.body).toMatchObject(expectedResponse)
-});
-
-it("PUT /jumplings/:id should return the person object with the edited name", async () => {
-    const updatedJumpling = { "name": "Jeff This has been EDITED" };
-    const expectedResponse = { "id": 1, "name": "Jeff This has been EDITED" };
-    const response = await request(app).put("/jumplings/1").send(updatedJumpling).expect(200);
-    expect(response.body).toMatchObject(expectedResponse)
-});
-
-it("DELETE /jumplings/:id should return the deleted person object", async () => {
-    const expectedDeletedResponse = { "id": 1, "name": "Jeff This has been EDITED" };
-    const response = await request(app).delete("/jumplings/1").expect(200);
-    expect(response.body).toMatchObject(expectedDeletedResponse)
-});
-
-it("GET /jumplings/presenter should return a randomly chosen person from the jumplings array", async () => {
-    const response = await request(app).get("/jumplings/presenter").expect(200);
-    expect(Object.keys(response.body).length).toEqual(2) // instead of { "id": 2, "name": "xxx" } as seen in question paper, just test that the response.body.length = 1 (aka returns an object)
-}); */
--- src/routes/jumplings.routes.js
+++ src/routes/jumplings.routes.js
@@ -30,19 +30,29 @@ const requireJsonContent = (req, res, next) => {
     }
   };
 
-router.param("id", (req, res, next, id) => {
-  // do whatever you like with the id, e.g. looking for a particular jumpling person object
-  // you can put selectedJumpling object inside the request object
-  // req.params.id is a string (i.e "1"), hence need to parseInt()
-  let selectedJumpling = jumplings.find((jumpling) => jumpling.id === parseInt(req.params.id)); 
-  req.selectedJumpling = selectedJumpling;
-  next();
-});
 
-router.param("name", (req, res, next, id) => {
-    let selectedJumplingByName = jumplings.find((jumpling) => jumpling.name === req.params.name);
-    req.selectedJumplingByName = selectedJumplingByName;
+router.param("id", async (req, res, next, id) => {
+  try {
+    const selectedJumpling = await Jumpling.findById(req.params.id);
+    req.selectedJumpling = selectedJumpling;
     next();
+  } catch (err) {
+      next(err);
+  }
+}); 
+
+router.param("name", async (req, res, next, name) => {
+    try {
+      const jumplings = await Jumpling.find({}); //find all the documents in the model, and it will be stored in an array
+      let selectedJumplingByName = jumplings.find((jumpling) => jumpling.name === req.params.name);
+      req.selectedJumplingByName = selectedJumplingByName;
+      next();
+    } catch (err) {
+        next(err);
+    }
+    //let selectedJumplingByName = jumplings.find((jumpling) => jumpling.name === req.params.name);
+    //req.selectedJumplingByName = selectedJumplingByName;
+    //next();
   });
 
 // 3) ROUTES
@@ -57,15 +67,12 @@ router.get("/", async (req, res, next) => {
 
 router.post("/", requireJsonContent, async (req, res, next) => { // dont forget put next here
     try {
-      let newJumpling = {
-        name: req.body.name
-      };
-      let jumpling = await new Jumpling(req.body);
-      jumpling = await jumpling.save();
-      res.status(201).json(jumpling);
+      const jumpling = await new Jumpling(req.body);
+      const newJumpling = await jumpling.save();
+      res.status(201).json(newJumpling);
     } catch (err) {
       next(err);
-    }
+    } 
 
     /* let newJumpling = {
         id: jumplings.length + 1,
@@ -85,20 +92,38 @@ router.post("/", requireJsonContent, async (req, res, next) => { // dont forget 
     } */
 });
 
-// this answer line 43 - 51 is for question 6 => however since /presenter will clash with /:name => the machine will see 'presenter' as a 'name', hence need to put above name to detect it
-router.get("/presenter", (req, res) => {
-    let randomNumberIndex = Math.floor(Math.random() * (jumplings2.length)) + 1; //generate a random number from 1 to 3
+// StackOverFlow how to select random jumpling document from the Jumpling Model/Collection: https://stackoverflow.com/questions/14644545/random-document-from-a-collection-in-mongoose
+router.get("/presenter", async (req, res) => {
+    await Jumpling.countDocuments({}).exec(function (err, count) {
+      let randomNumber = Math.floor(Math.random() * (count)); //generate a random number from 0 to 2
+      Jumpling.findOne().skip(randomNumber).exec(function (err, result) {
+          res.status(200).json(result) //result will contain the randomly selected Jumpling
+      })
+      
+    });
+    // this answer line 43 - 51 is for question 6 => however since /presenter will clash with /:name => the machine will see 'presenter' as a 'name', hence need to put above name to detect it
+    /* let randomNumberIndex = Math.floor(Math.random() * (jumplings2.length)) + 1; //generate a random number from 1 to 3
     let selectedJumpling = jumplings2.find((jumpling) => jumpling.id === randomNumberIndex)
-    res.status(200).json(selectedJumpling)
+    res.status(200).json(selectedJumpling) */
 })
 
-router.get("/:name", (req, res)=> {
+router.get("/:name", async (req, res)=> {
     res.status(200).json(req.selectedJumplingByName);
 })
 
-router.put("/:id", requireJsonContent, (req, res, next) => {
+// this function is obsolete since first we no longer hardcode the id in the individual jumpling object
+/* router.get("/:id", async (req, res)=> {
+  res.status(200).json(req.selectedJumpling);
+}) */
 
-
+router.put("/:id", requireJsonContent, async (req, res, next) => {
+    try {
+      const jumpling = await Jumpling.findByIdAndUpdate(req.selectedJumpling._id, req.body, {new: true, runValidators:true}); // In the Jumpling model, find an instance/document with that ID, and update it with the 2nd parameters. Jumpling model contains all your model instances/documents
+      res.status(200).json(jumpling);
+    } catch (err) {
+        next(err);
+    }
+    
     // JOI VALIDATION DELETED
     /* const validation = validateJumpling(req.body);
     if (validation.error) {
@@ -112,10 +137,17 @@ router.put("/:id", requireJsonContent, (req, res, next) => {
       } */
 })
 
-router.delete("/:id", (req, res) => {
-    let index = jumplings.indexOf(req.selectedJumpling);
-    jumplings.splice(index, 1);
-    res.status(200).json(req.selectedJumpling);
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const deletedJumpling = await Jumpling.findByIdAndDelete(req.selectedJumpling._id);
+    res.status(200).json(deletedJumpling)
+
+  } catch (err) {
+    next(err)
+  }
+    //let index = jumplings.indexOf(req.selectedJumpling);
+    //jumplings.splice(index, 1);
+    //res.status(200).json(req.selectedJumpling);
 })
 
 module.exports = router;
